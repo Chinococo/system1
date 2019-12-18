bdce79b517201964eb26e0b6e147050c377e5fc7
chinococo <garyliuyahoocom384@gmail.com>
2019-12-19 06:27:46 +0800

    v0.12(beta)

--- app/src/main/java/com/example/health_system/enter_score_screen.java
+++ app/src/main/java/com/example/health_system/enter_score_screen.java
@@ -41,15 +41,20 @@ import java.util.List;
 import java.util.Map;
 
 public class enter_score_screen extends AppCompatActivity {
+    List<String> item = new ArrayList<>();
+    TextView grade1, grade2, grade3, grade4, grade5;
     int pos = -1;
     String no;
-    score_struct[] score_s1 = new score_struct[10];
+    List<String> op = new ArrayList<>();
+    Spinner opeator;
+    List<String> test = new ArrayList<>();
+    score_struct[] score_s1 = new score_struct[20];
     EditText enter1, enter2, enter3, enter4, enter5;
     Button enter;
     Calendar calendar = Calendar.getInstance();
-    String today;
+    String today, account_name;
     Intent intent;
-    List<String> get;
+    List<String> get = new ArrayList<>();
     Spinner choose;
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
     Map<String, List> c = new HashMap<>();
@@ -60,17 +65,34 @@ public class enter_score_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_score_screen);
-
-        upload("10");
         intent = getIntent();
         setup();
         getdata(no);
         getnowdata(no);
         event();
+        opeator.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+                no = op.get(position);
+                if (!no.equals("請選擇")) {
+                    clear_Alldata();
+                    getdata(no);
+                    getitem(no);
+                } else {
+                    do2();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("test1", String.valueOf(score_s1[0].getScore().size()));
                 for (int i = 0; i < get.size(); i++)
                     databaseReference.child("no").child(no).child(today).child(score_s1[i].getName()).setValue(score_s1[i].getScore());
                 nofition("上傳完成");
@@ -79,12 +101,22 @@ public class enter_score_screen extends AppCompatActivity {
 
     }
 
+    private void do2() {
+        get.clear();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, get);
+        choose.setAdapter(adapter);
+
+    }
+
     private void getdata(String no) {
         databaseReference.child("no").child(no).child("class").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                get = (List<String>) dataSnapshot.getValue();
-                do1();
+                if (dataSnapshot.getValue() != null) {
+                    get = (List<String>) dataSnapshot.getValue();
+                    do1();
+                }
+
                 //choose.setAdapter();
             }
 
@@ -97,15 +129,28 @@ public class enter_score_screen extends AppCompatActivity {
 
     }
 
+    void clear_Alldata() {
+        enter1.setText("");
+        enter2.setText("");
+        enter3.setText("");
+        enter4.setText("");
+        enter5.setText("");
+
+    }
+
     void do1() {
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, get);
         choose.setAdapter(adapter);
+        Log.d("re", String.valueOf(get.size()));
         for (int i = 0; i < get.size(); i++) {
-            //Log.d("re",get.get(i));
+            Log.d("re", get.get(i));
             score_s1[i].setName(get.get(i));
-            for (int x = 0; x < 5; x++) {
-                score_s1[i].score.add(0);
-            }
+            if (score_s1[0].getScore().size() <= 5)
+                for (int x = 0; x < 5; x++) {
+                    score_s1[i].score.add(0.0);
+                }
+            Log.d("test1", String.valueOf(score_s1[0].getScore().size()));
+
         }
 
 
@@ -124,6 +169,26 @@ public class enter_score_screen extends AppCompatActivity {
     }//上傳程式
 
     void setup() {
+        grade1 = findViewById(R.id.grade1);
+        grade2 = findViewById(R.id.grade2);
+        grade3 = findViewById(R.id.grade3);
+        grade4 = findViewById(R.id.grade4);
+        grade5 = findViewById(R.id.grade5);
+        opeator = findViewById(R.id.opeator);
+        no = intent.getStringExtra("no");
+        account_name = intent.getStringExtra("account");
+        getitem(no); //拿評分項目
+        op.add("請選擇");
+        for (int j = 1; j <= 24; j++) {
+            op.add(String.valueOf(j));
+            if (j == 24) {
+                if (account_name.equals("teacher")) {
+                    opeator.setVisibility(View.VISIBLE);
+                    ArrayAdapter<String> op_adpart = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, op);
+                    opeator.setAdapter(op_adpart);
+                }
+            }
+        }
 
         today = Integer.toString(calendar.get(Calendar.YEAR));
         if (calendar.get(Calendar.MONTH) + 1 < 10)
@@ -132,11 +197,12 @@ public class enter_score_screen extends AppCompatActivity {
         if (calendar.get(Calendar.DAY_OF_MONTH) < 10)
             today += "0";
         today += Integer.toString(calendar.get(Calendar.DAY_OF_MONTH));
-        no = intent.getStringExtra("no");
+
+
         nofition("登入成功");
         //nofition(no);//測試
-        for (int i = 0; i < 6; i++)
-        score_s1[i] = new score_struct();
+        for (int i = 0; i < 20; i++)
+            score_s1[i] = new score_struct();
         choose = findViewById(R.id.enter_score_spinner);
         enter = findViewById(R.id.enter_scorce_btn);
         enter1 = findViewById(R.id.enter_score_edit1);
@@ -146,12 +212,41 @@ public class enter_score_screen extends AppCompatActivity {
         enter5 = findViewById(R.id.enter_score_edit5);
     }//初始化變數
 
+    private void getitem(String no) {
+        if (Integer.parseInt(no) >= 1 && Integer.parseInt(no) <= 12)
+            no = "1~12";
+        else if (Integer.parseInt(no) >= 13 && Integer.parseInt(no) <= 17)
+            no = "13~17";
+        else if (Integer.parseInt(no) >= 18 && Integer.parseInt(no) <= 24)
+            no = "18~24";
+        else
+            no = "opeator";
+        databaseReference.child("no").child(no).child("item").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() != null) {
+                    item = (ArrayList<String>) dataSnapshot.getValue();
+                    grade1.setText(item.get(1));
+                    grade2.setText(item.get(2));
+                    grade3.setText(item.get(3));
+                    grade4.setText(item.get(4));
+                    grade5.setText(item.get(5));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     void nofition(String data) {
         Toast.makeText(this, data, Toast.LENGTH_LONG).show();
     }
 
     private void getnowdata(String no) {
-        enter5.setText(today);
+        //enter5.setText(today);
         databaseReference.child("no").child(no).child(today).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -159,7 +254,7 @@ public class enter_score_screen extends AppCompatActivity {
                 if (dataSnapshot.getValue() == null) {
                 } else {
                     for (int i = 0; i < get.size(); i++)
-                        score_s1[i].SETSCORE((List<Integer>) dataSnapshot.child(get.get(i)).getValue());
+                        score_s1[i].SETSCORE((List<Double>) dataSnapshot.child(get.get(i)).getValue());
                 }
             }
 
@@ -180,7 +275,7 @@ public class enter_score_screen extends AppCompatActivity {
                 pos = position;
                 //Log.d("ERROR",String.valueOf(pos));
                 if (position - 1 >= 0) {
-                    List<Integer> temp = score_s1[pos].getScore();
+                    List<Double> temp = score_s1[pos].getScore();
                     enter1.setText(String.valueOf(temp.get(0)));
                     enter2.setText(String.valueOf(temp.get(1)));
                     enter3.setText(String.valueOf(temp.get(2)));
@@ -206,9 +301,9 @@ public class enter_score_screen extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
                 if (!enter1.getText().toString().equals("") && pos >= 0) {
-                    score_s1[pos].setScore(0, Integer.parseInt(enter1.getText().toString()));
-                    if(enter1.getText().toString().equals("0"))
-                    enter1.setText("");
+                    score_s1[pos].setScore(0, Double.valueOf(enter1.getText().toString()));
+                    if (enter1.getText().toString().equals("0.0"))
+                        enter1.setText("");
                 }
 
             }
@@ -227,8 +322,8 @@ public class enter_score_screen extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
                 if (!enter2.getText().toString().equals("") && pos >= 0) {
-                    score_s1[pos].setScore(1, Integer.parseInt(enter2.getText().toString()));
-                    if(enter2.getText().toString().equals("0")) enter2.setText("");
+                    score_s1[pos].setScore(1, Double.valueOf(enter2.getText().toString()));
+                    if (enter2.getText().toString().equals("0.0")) enter2.setText("");
                 }
 
             }
@@ -247,8 +342,8 @@ public class enter_score_screen extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
                 if (!enter3.getText().toString().equals("") && pos >= 0) {
-                    score_s1[pos].setScore(2, Integer.parseInt(enter3.getText().toString()));
-                    if(enter3.getText().toString().equals("0"))
+                    score_s1[pos].setScore(2, Double.valueOf(enter3.getText().toString()));
+                    if (enter3.getText().toString().equals("0.0"))
                         enter3.setText("");
                 }
 
@@ -268,8 +363,8 @@ public class enter_score_screen extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
                 if (!enter4.getText().toString().equals("") && pos >= 0) {
-                    score_s1[pos].setScore(3, Integer.parseInt(enter4.getText().toString()));
-                    if(enter4.getText().toString().equals("0"))
+                    score_s1[pos].setScore(3, Double.valueOf(enter4.getText().toString()));
+                    if (enter4.getText().toString().equals("0.0"))
                         enter4.setText("");
                 }
 
@@ -289,8 +384,8 @@ public class enter_score_screen extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
                 if (!enter5.getText().toString().equals("") && pos >= 0) {
-                    score_s1[pos].setScore(4, Integer.parseInt(enter5.getText().toString()));
-                    if(enter5.getText().toString().equals("0"))
+                    score_s1[pos].setScore(4, Double.valueOf(enter5.getText().toString()));
+                    if (enter5.getText().toString().equals("0.0"))
                         enter5.setText("");
                 }
 
