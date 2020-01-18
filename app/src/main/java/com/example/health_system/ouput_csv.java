8ef1eecae5a7960842a2e618e2715409111cb9bc
chinococo <garyliuyahoocom384@gmail.com>
2020-01-18 21:42:58 +0800

    v0.2master(beta)

--- app/src/main/java/com/example/health_system/ouput_csv.java
+++ app/src/main/java/com/example/health_system/ouput_csv.java
@@ -2,13 +2,26 @@ package com.example.health_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ouput_csv extends AppCompatActivity {
-
+    Button  test;
+   
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ouput_csv);
+        test=findViewById(R.id.test1235456789);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
